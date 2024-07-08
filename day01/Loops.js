76403ca6d19a291b2b50e6f6a7f552d6b090fc53
Ahmed Sdiri <74168982+MedSdiri@users.noreply.github.com>
2024-07-08 17:06:03 -0500

    JavaScript day 01

---
+++ day01/Loops.js
@@ -0,0 +1,27 @@
+
+for (let i = 0; i < 5; i++) {
+  console.log("Wooden Spoon");
+}
+
+console.log("-----------------------------------------------------");
+
+for (let i = 5; i <= 15; i++){
+    console.log(i);
+}
+
+console.log("-----------------------------------------------------");
+
+for (let i = 0; i < 7; i++) {
+    console.log('Hello Ahmed');
+    console.log('Hello Programmer');
+    
+}
+
+console.log("------------------------------------------");
+
+for (let i = 1; i < 11; i++) {
+    if(i === 3 || i === 7){
+        continue; // skips
+    }
+    console.log(i);
+}
